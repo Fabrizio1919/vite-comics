b993320e1add454558e76d3b6dd7d66876266dd2
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-04-13 17:36:49 +0200

    Add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,61 @@
 <script>
+import AppHeader from "./components/AppHeader.vue"
+export default {
+  components: {
+    AppHeader
+  }
+  /* data() {
+    return {
+      menuItems: [
+      //   {
+      //     title: "Characters",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "Comics",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "Movies",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "TV",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "Games",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "Collectibles",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "Videos",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "Fans",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "News",
+      //     url: "#",
+      //   },
+      //   {
+      //     title: "Shop",
+      //     url: "#",
+      //   },
+       ]
+    }
+  } */
+}
 </script>
 
 <template>
-  <header>
-    <div class="logo_sm">
-      <img src="" alt="">
-    </div>
-    <nav>
-        <ul>
-            <li><a href=""></a></li>
-        </ul>
-    </nav>
 
-
-
-  </header>
+<AppHeader></AppHeader>
 
 
 
@@ -30,5 +71,8 @@
 </template>
 
 <style>
+a {
+  text-decoration: none;
+}
 
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+    data() {
+        return {
+            menuItems: [
+                {
+                    title: "Characters",
+                    url: "#",
+                },
+                {
+                    title: "Comics",
+                    url: "#",
+                },
+                {
+                    title: "Movies",
+                    url: "#",
+                },
+                {
+                    title: "TV",
+                    url: "#",
+                },
+                {
+                    title: "Games",
+                    url: "#",
+                },
+                {
+                    title: "Collectibles",
+                    url: "#",
+                },
+                {
+                    title: "Videos",
+                    url: "#",
+                },
+                {
+                    title: "Fans",
+                    url: "#",
+                },
+                {
+                    title: "News",
+                    url: "#",
+                },
+                {
+                    title: "Shop",
+                    url: "#",
+                },
+            ]
+        }
+    }
+}
+
+</script>
+
+<template>
+    <header class="d-flex align-items-center justify-content-center">
+        <div class="container d-flex justify-content-between align-items-center h-100">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="DC Logo">
+            </div>
+            <ul class="items d-flex m-0 p-0 h-100">
+                <li v-for="(item, index) in menuItems" :class="index === 0 ? 'active' : ''"
+                    class="d-flex align-items-center mx-3 h-100 ">
+                    <a class="text-dark" :href="item.url">{{ item.title.toUpperCase() }}</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style></style>
